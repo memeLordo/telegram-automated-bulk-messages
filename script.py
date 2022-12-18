e6469cb9000d5a88cf2d474cbb535bef730a1f57
Sima <100208922+memeLordo@users.noreply.github.com>
2022-12-18 18:30:56 +0300

    Some quality of life fixes

--- script.py
+++ script.py
@@ -1,29 +1,43 @@
-# Program to send bulk customized messages through Telegram Desktop application
-# Author @inforkgodara
-
 import pyautogui
+import pyperclip
 import pandas
 import time
+from subprocess import Popen
 
-excel_data = pandas.read_excel('Recipients data.xlsx', sheet_name='Recipients')
 
+def paste(text):
+    pyperclip.copy(text)
+    pyautogui.hotkey('ctrl', 'v')
+
+
+telegramPath = 'C:\\Users\\Sima Forest\\AppData\\Roaming\\Telegram Desktop\\Telegram.exe'
+telegram = Popen(telegramPath)
+while telegram.poll() is None:
+    telegram.wait()
+
+excel_data = pandas.read_excel('Recipients data.xlsx', sheet_name='Recipients').fillna('')
+
+print(excel_data)
 count = 0
-
-time.sleep(3)
 
 for column in excel_data['Username'].tolist():
 
-      pyautogui.press('esc')
-      pyautogui.hotkey('ctrl', 'f')
-      time.sleep(1)
-      pyautogui.write(str(excel_data['Username'][count]));
-      pyautogui.press('enter')
-      time.sleep(2)
-      pyautogui.press('down')
-      pyautogui.press('enter')
-      pyautogui.write(str(excel_data['Message'][0]));
-      pyautogui.press('enter')
-      pyautogui.press('esc')
-      count = count + 1
+    if str(excel_data['Username'][count]) == '':
+        print('Empty line on ' + str(count + 1)+' number')
+    else:
+        pyautogui.press('esc')
+        pyautogui.press('esc')
+        time.sleep(0.5)
+        pyautogui.hotkey('ctrl', 'j')
+        time.sleep(1)
+        paste(str(excel_data['Username'][count]))
+        time.sleep(1)
+        pyautogui.press('enter')
+        time.sleep(1)
+        paste(excel_data['Message'][0])
+        time.sleep(1)
+        pyautogui.press('enter')
+    count = count + 1
 
+pyautogui.press('esc')
 print('The script executed successfully.')
