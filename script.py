3d07fe74533067ef02bb37e7a47fc156a82c4bc6
Sima <100208922+memeLordo@users.noreply.github.com>
2022-12-19 14:20:29 +0300

    Some quality of life fixes 2.0

--- script.py
+++ script.py
@@ -7,13 +7,13 @@ from subprocess import Popen
 
 def paste(text):
     pyperclip.copy(text)
-    pyautogui.hotkey('ctrl', 'v')
+    pyautogui.hotkey('ctrl', 'v', interval=0.25)
 
 
 telegramPath = 'C:\\Users\\Sima Forest\\AppData\\Roaming\\Telegram Desktop\\Telegram.exe'
 telegram = Popen(telegramPath)
-while telegram.poll() is None:
-    telegram.wait()
+# while telegram.poll() is None:
+#     telegram.wait()
 
 excel_data = pandas.read_excel('Recipients data.xlsx', sheet_name='Recipients').fillna('')
 
@@ -23,19 +23,20 @@ count = 0
 for column in excel_data['Username'].tolist():
 
     if str(excel_data['Username'][count]) == '':
-        print('Empty line on ' + str(count + 1)+' number')
+        print('Empty line on ' + str(count + 2) + ' number')
     else:
         pyautogui.press('esc')
         pyautogui.press('esc')
-        time.sleep(0.5)
-        pyautogui.hotkey('ctrl', 'j')
-        time.sleep(1)
+        time.sleep(0.75)
+        pyautogui.hotkey('ctrl', 'j', interval=0.25)
+        time.sleep(0.75)
         paste(str(excel_data['Username'][count]))
-        time.sleep(1)
+        time.sleep(0.75)
         pyautogui.press('enter')
-        time.sleep(1)
+        time.sleep(0.75)
         paste(excel_data['Message'][0])
-        time.sleep(1)
+        # print(excel_data['Message'][0])
+        time.sleep(0.75)
         pyautogui.press('enter')
     count = count + 1
 
